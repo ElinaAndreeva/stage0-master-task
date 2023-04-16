3438cc8d1fb8a4fe848df9950c60eee50248ddff
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-16 18:52:06 +0400

    fixed test

--- src/test/java/com/epam/OOP/AnimalTest.java
+++ src/test/java/com/epam/OOP/AnimalTest.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import com.epam.mjc.stage0.Animal;
 
 public class AnimalTest {
 
@@ -61,7 +62,7 @@ public class AnimalTest {
             Field hasFurField = Animal.class.getDeclaredField("hasFur");
             hasFurField.setAccessible(true);
 
-            Animal animal = (Animal) constructor.newInstance("someColor", 123123, true);
+            com.epam.mjc.stage0.Animal animal = (Animal) constructor.newInstance("someColor", 123123, true);
 
             Assertions.assertEquals("someColor", (String) colorField.get(animal), "Not correct set up for color field");
             Assertions.assertEquals(123123, (int) numberOfPawsField.get(animal), "Not correct set up for numberOfPawsField field");
